c6f18ee6a0931a9e7bae21f87d4f987728c3d78a
kailaneribeiro <dasilvaribeirokailane@gmail.com>
2024-10-16 17:37:27 -0300

    fim

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectDB= async () =>{
-    mongoose.connect('mongodb+srv://${process.env_DB_USER}:${process.env.DB_PASSWoRD}@cluster0.8os58.mongodb.net/oficina')
+    mongoose.connect(`${process.env.URI}`)
     .then(() => console.log('Connectado ao MondoDB'))
     .catch((error) => console.log(error))
 }
